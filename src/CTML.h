ccc7bf90d8d151391304fed1eaf32b69fa2a9ad5
davidlynch-sd <davite3@gmail.com>
2022-04-29 18:18:14 +0100

    Beginning to define syntax

--- src/CTML.h
+++ src/CTML.h
@@ -2,13 +2,33 @@
 
 void ctmlCompile(FILE *input, FILE *output)
 {
+
+	
+	
+
+	int opcount = 0;
+	int clcount = 0;
 	while(!feof(input))
 	{	
-		printf("%c",fgetc(input));
+		char c = fgetc(input);
 
-					
+		if(c == '{')
+			opcount++;
+		if(c == '}')
+			clcount++;
+	
+	}
+	
+	
+	//syntax checking
+	if(opcount == clcount)
+	{
+		elStack Stack;	
 
-
+		Stack.len = opcount;
+		Stack.names = malloc(sizeof(char*) * Stack.len);
 	}
 
+
+	printf("{:%d}:%d",opcount,clcount);
 }
